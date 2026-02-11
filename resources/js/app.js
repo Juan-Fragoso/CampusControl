1295678a990ff6435fabf2b2f78e027ba5f9d046
Juan Fragoso <jftrix01@gmail.com>
2026-02-11 00:26:34 -0600

    add css styles

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,6 +1,8 @@
 import './bootstrap';
 
 import Alpine from 'alpinejs';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 window.Alpine = Alpine;
 
